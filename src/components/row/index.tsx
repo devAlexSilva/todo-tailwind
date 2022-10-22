b0f45d9047e6f5cc2eb39f87870d731470f36404
Alex Silva <9apsilva@gmail.com>
2022-10-22 00:48:49 -0300

    feat: add remove and update task

--- src/components/row/index.tsx
+++ src/components/row/index.tsx
@@ -1,27 +1,27 @@
 export type Todo = {
-  id?: string;
-  task?: string;
-  isCompleted?: boolean;
+  id: string;
+  task: string;
+  isCompleted: boolean;
+  deleteTodo: (id: string) => void;
+  updateTodo: (id: string) => void;
 };
 
 export const Row = (props: Todo) => {
-  const { id, task, isCompleted } = props;
+  const { id, task, isCompleted, deleteTodo, updateTodo } = props;
   return (
-    <>
+    <article>
       <p>{task}</p>
       <div>
-        <button
-          aria-label="deletar um to-do"
-          onClick={() => console.log("deleted")}
-        >
+        <button aria-label="deletar um to-do" onClick={() => deleteTodo(id)}>
           X
         </button>
         <input
           type="checkbox"
+          aria-label="marcar to-do como completo"
           checked={isCompleted}
-          onChange={() => console.log("changed")}
+          onChange={() => updateTodo(id)}
         />
       </div>
-    </>
+    </article>
   );
 };
--- src/components/todo/index.tsx
+++ src/components/todo/index.tsx
@@ -1,10 +1,37 @@
-import { Row } from "../row"
-import { data } from "../../mock/todo"
+import { Row } from "../row";
+import { data } from "../../mock/todo";
+import { useState } from "react";
 
 export const Todo = () => {
-    return (
-        <>
-        {data.map(item => <Row key={item.id} {...item}/>)}
-        </>
-    )
-}
+  const [todos, setTodos] = useState(data);
+
+  const updateTodo = (id: string) => {
+    const updatedTodo = todos.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          isCompleted: !item.isCompleted,
+        };
+      }
+      return item;
+    });
+    setTodos(updatedTodo);
+  };
+
+  const deleteTodo = (id: string) => {
+    const allTodos = todos.filter((item) => item.id !== id);
+    setTodos(allTodos);
+  };
+
+  return (
+    <section about="seção dos to-do">
+      <ul>
+        {todos.map((item) => (
+          <li key={item.id}>
+            <Row {...item} deleteTodo={deleteTodo} updateTodo={updateTodo} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
